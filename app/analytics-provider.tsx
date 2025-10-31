e4e4e7be6b907e8ed904974e9c65cce9d9a9f8db
Pratham Mishra <prathammishra@Prathams-MacBook-Air.local>
2025-10-31 23:12:00 +0530

    jfjf

--- app/analytics-provider.tsx
+++ app/analytics-provider.tsx
@@ -9,7 +9,7 @@ export default function AnalyticsProvider() {
   const searchParams = useSearchParams()
 
   useEffect(() => {
-    const url = pathname + searchParams.toString()
+    const url = pathname + (searchParams ? `?${searchParams.toString()}` : '')
     gtag.pageview(url)
   }, [pathname, searchParams])
 
