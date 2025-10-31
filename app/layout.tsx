a2d8705c5813901dec4b0be6e9d499a55232a7be
PRATHAM MISHRA <114295564+pratham99013@users.noreply.github.com>
2025-10-31 22:14:04 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
+
 import "./globals.css"
 
 const _geist = Geist({ subsets: ["latin"] })
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`font-sans antialiased bg-background text-foreground`}>
         {children}
-        <Analytics />
+
       </body>
     </html>
   )
